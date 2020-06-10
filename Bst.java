02c6d7db22d94ffc369e75f86afb58d501ef6e6d
Rogelio <rogercp1234@yahoo.com>
2020-06-09 19:21:36 -0700

    BST implentation

--- Bst.java
+++ Bst.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Bst
 {
@@ -80,22 +80,17 @@ public class Bst
 
             }
 
-
         }
 
 
-        public Node find(int value)
+        public boolean find(int value)
         {
 
-            if(this.root == null)
-            {
-                return false;
-            }
-
+        
             Node currentNode = this.root;
             boolean found = false;
 
-            while(currentNode.value != null && found == false)
+            while(found == false)
             {
                 if( value < currentNode.value)
                 {
@@ -109,25 +104,141 @@ public class Bst
                 }
             }
 
-            if( found == false)
-            {
-                return this;
-            }
-
-            System.out.println(currentNode.getValue());
-            return currentNode;
-
+            return found;
 
         }
 
-      
 
+
+        public ArrayList<Node> Breath_First_Search()
+        {
+
+            Node currentNode = this.root;
+
+            ArrayList<Node> data=new ArrayList<Node>();
+            ArrayList<Node> queue=new ArrayList<Node>();
+
+            queue.add(currentNode);
+
+            while(queue.size() > 0)
+            {
+                Node node = queue.remove(0);
+                data.add(node);
+                if(node.left != null)
+                {
+                    queue.add(node.left);
+                }
+
+                if(node.right != null)
+                {
+                    queue.add(node.right);
+                } 
+            }
+
+            for (Node n : data)
+            {
+                System.out.println(n.value);
+            }
+
+            return data;
+        }
+
+        public ArrayList<Node> DFSPreOrder()
+        {
+            ArrayList<Node> data=new ArrayList<Node>();
+                
+                traverse1(this.root);
+
+                 for (Node n : data)
+                    {
+                        System.out.println(n.value);
+                    }
+
+                return data;
+        }
+
+         public void traverse1(Node node)
+                {
+
+                    data.add(node);
+                    if(node.left != null)
+                    {
+                        traverse1(node.left);
+                    }
+                    if(node.right != null)
+                    {
+                        traverse1(node.right);
+                    } 
+
+
+                }
+
+        public ArrayList<Node> DFSInOrder()
+        {            
+            ArrayList<Node> data=new ArrayList<Node>();
+                 
+
+                traverse2(this.root);
+
+                 for (Node n : data)
+                    {
+                        System.out.println(n.value);
+                    }
+                    
+                return data;
+
+        }
+         public void traverse2(Node node)
+                {
+                    if(node.left != null) 
+                    {
+                        traverse2(node.left);
+                    }
+                    data.add(node);
+                    if(node.right != null)
+                    {
+                        traverse2(node.right);
+                    } 
+
+                }
+
+        public ArrayList<Node> DFSPostOrder()
+        {            
+            ArrayList<Node> data=new ArrayList<Node>();
+                 
+               
+                traverse3(this.root);
+
+                 for (Node n : data)
+                    {
+                        System.out.println(n.value);
+                    }
+                    
+                return data;
+
+        }
+        public void traverse3(Node node)
+            {
+                    
+                if(node.left != null)
+                {
+                    traverse3(node.left);
+                 } 
+                if(node.right != null)
+                {
+                    traverse3(node.right);
+                } 
+                 data.add(node);
+
+
+                }
+
+    
 
 }
 
     public void start() {
 
-        // Node rootNode = new Node(42);
 
         BinarySearchTree myBst = new BinarySearchTree(null);
 
@@ -141,9 +252,20 @@ public class Bst
         myBst.insert(9);
         myBst.insert(65);
 
-        myBst.find(23);
+        boolean myResult = myBst.find(9);
+        System.out.println(myResult);
 
-        System.out.println("lets see");
+        myBst.Breath_First_Search();
+
+        myBst.DFSPreOrder();
+
+
+
+
+
+
+
+
 
 
         }
