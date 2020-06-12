9e3d913baee464cb67c2b695a0c5eafd12abdd6b
Rogelio <rogercp1234@yahoo.com>
2020-06-11 21:51:22 -0700

    string reverse in place algo

---
+++ codeProblems/StringReverse.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+
+
+public class StringReverse
+{
+
+
+
+    public void string_reverse_in_place(char s[])
+    {
+       
+        int left_index = 0;
+        int right_index = s.length-1;
+
+
+        while(left_index < right_index)
+        {
+            char temp = s[right_index];
+            s[right_index] = s[left_index];
+            s[left_index] = temp;
+
+            left_index = left_index + 1;
+            right_index = right_index - 1;
+
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args)
+    {
+
+        StringReverse program = new StringReverse();
+    
+        char myList[] = {'h','i','s','t','o','r','y'};
+
+        program.string_reverse_in_place(myList);
+
+        System.out.println(myList);
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
