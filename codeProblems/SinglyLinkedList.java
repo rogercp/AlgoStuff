76877197473cad3f2e02616b7cbcb762205ea651
rogercp <rogercp1234@yahoo.com>
2020-08-06 19:04:23 -0700

    reverse singly linked list

--- codeProblems/SinglyLinkedList.java
+++ codeProblems/SinglyLinkedList.java
@@ -138,6 +138,29 @@ public class LinkedList {
     } 
   
 
+    public static Node reverse(Node head) {
+    Node currentNode = head;
+    Node previousNode = null;
+    Node nextNode = null;
+
+    // until we have 'fallen off' the end of the list
+    while (currentNode != null) {
+
+        // copy a pointer to the next element
+        // before we overwrite currentNode.next
+        nextNode = currentNode.next;
+
+        // reverse the 'next' pointer
+        currentNode.next = previousNode;
+
+        // step forward in the list
+        previousNode = currentNode;
+        currentNode = nextNode;
+    }
+
+    return previousNode;
+}
+
     public static void main(String[] args) 
     { 
         LinkedList list = new LinkedList(); 
