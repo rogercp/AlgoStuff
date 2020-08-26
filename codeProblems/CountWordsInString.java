33712a955c089ec0a8d46cfe42b893470bcb6f6b
rogercp <rogercp1234@yahoo.com>
2020-08-25 23:04:19 -0700

    added scanner

--- codeProblems/CountWordsInString.java
+++ codeProblems/CountWordsInString.java
@@ -1,12 +1,18 @@
 import java.util.HashMap;
- 
+import java.util.Scanner;
+
+
 public class CountWordsInString {
 
 
     public static void main(String[] args) {
-        String str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
+        // String str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
 
-        String[] split = str.split(" ");
+        Scanner in = new Scanner(System.in);
+
+        String myString = in.nextLine();
+
+        String[] split = myString.split(" ");
 
          
         HashMap<String,Integer> map = new HashMap<String,Integer>();
@@ -21,7 +27,7 @@ public class CountWordsInString {
                 map.put(split[i], 1);
             }
         }
-        
+
         System.out.println(map);
     }
 
