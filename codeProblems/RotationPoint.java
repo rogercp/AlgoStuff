02d40d1527bb32e7f2889f6fc8c7383950f8e214
rogercp <rogercp1234@yahoo.com>
2020-08-28 23:11:15 -0700

    bug fix on rotation point

--- codeProblems/RotationPoint.java
+++ codeProblems/RotationPoint.java
@@ -15,23 +15,17 @@ public class RotationPoint
 
     while (floorIndex < ceilingIndex) {
 
-        // guess a point halfway between floor and ceiling
         int guessIndex = floorIndex + ((ceilingIndex - floorIndex) / 2);
 
-        // if guess comes after first word or is the first word
         if (words[guessIndex].compareTo(firstWord) >= 0) {
-            // go right
             floorIndex = guessIndex;
         } else {
-            // go left
             ceilingIndex = guessIndex;
         }
 
-        // if floor and ceiling have converged
         if (floorIndex + 1 == ceilingIndex) {
 
-            // between floor and ceiling is where we flipped to the beginning
-            // so ceiling is alphabetically first
+   
             break;
         }
     }
