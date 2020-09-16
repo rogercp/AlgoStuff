ede2d3850a994ebe1755d552458c3d08fdc5080d
rogercp <rogercp1234@yahoo.com>
2020-09-15 19:18:14 -0700

    order checking alog

--- codeProblems/OrderChecker.java
+++ codeProblems/OrderChecker.java
@@ -1,18 +1,6 @@
 import java.util.ArrayList;
 
 
-// # I have two registers: one for take-out orders, and the other for the other folks eating 
-// # inside the cafe. All the customer orders get combined into one list for the kitchen, where 
-// # they should be handled first-come, first-served.
-
-// # Recently, some customers have been complaining that people who placed orders after them are 
-// # getting their food first. Yikes—that's not good for business!
-
-// # To investigate their claims, one afternoon I sat behind the registers with my laptop and recorded:
-
-// # The take-out orders as they were entered into the system and given to the kitchen. (take_out_orders)
-// # The dine-in orders as they were entered into the system and given to the kitchen. (dine_in_orders)
-// # Each customer order (from either register) as it was finished by the kitchen. (served_orders)
 
 
 public class OrderChecker
