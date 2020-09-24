05ddef931d763d890fcf8feed0589244d09c4970
rogercp <rogercp1234@yahoo.com>
2020-09-23 19:16:08 -0700

    words rotation point comments binary search for index

--- codeProblems/RotationPoint.java
+++ codeProblems/RotationPoint.java
@@ -82,27 +82,64 @@ public class RotationPoint
   public static int findRotationPointBinarySearch(String[] words) 
   {
 
+  		//final keyword means it can only be assigned initally and cannot be 
+  		//reassigned 
+  		//we are holding the first word here
 	  	final String firstWord = words[0];
 
+	  	//create two indexes a beginning and 
+	  	//one for the end or the size of the words array
 	    int floorIndex = 0;
-
 	    int ceilingIndex = words.length - 1;
 
+	    //loop through until the floorindex meets the cieling index
+	    //this is just a modified binary search
 	    while (floorIndex < ceilingIndex) {
 
+
+	    	//halfwaypoint between the current ceiling index and the current floor index
 	        int guessIndex = floorIndex + ((ceilingIndex - floorIndex) / 2);
 
+	        System.out.println("guessindex---");
+	        System.out.println(words[guessIndex]);
+
+//*compareTo method looks at each string letter per letter and if you have two words like ape nad apple it makes the comparison at the 
+// first instance it can which would be the e and the p	and either returns a positive or negative number 
+
+	        //lexographically if the guessindex word is after the first word or quals the first word alphabetically
+	        //do the block you get a positive numbers 
 	        if (words[guessIndex].compareTo(firstWord) >= 0) {
+
+	        	System.out.println("if---");
+	        	System.out.println(words[guessIndex]);
+
+	        	//we move the new floor index to what is now the halfway point 
+	        	//because it is not alphabetically before the current word 
 	            floorIndex = guessIndex;
-	        } else {
+
+	        } 
+
+	        //lexographically if the guessindex word is before the first word alphabetically
+	        //do the block you get a negative number
+	        else {
+
+			System.out.println("else---");
+	       	 System.out.println(words[guessIndex]);
+
+	       	 	//we move the new cieling to be our current word 
 	            ceilingIndex = guessIndex;
 	        }
 
+
+	        //checking to see if we are getting close the convergence of floor and cieling indexes
+	        //if so break and return the current cieling
 	        if (floorIndex + 1 == ceilingIndex) {
 
+	        System.out.println("last block ran---");
 	   
 	            break;
 	        }
+
 	    }
 
 	    return ceilingIndex;
@@ -119,12 +156,12 @@ public class RotationPoint
 				
 		String[] words = new String[]{
 		  "ptolemaic", "retrograde", "supplant", "undulate", "xenoepist",
-            "asymptote", "babka", "banoffee", "engender", "karpatka", "othellolagkage"
+            "asymptote", "babka",
 		};
 
 		RotationPoint program = new RotationPoint();
 
-		int result = program.findRotationPoint(words);
+		int result = program.findRotationPointBinarySearch(words);
 
 
 		System.out.println(result);
