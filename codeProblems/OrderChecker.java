581e234fe321a7a6b95884c3a1e654770c2a9554
rogercp <rogercp1234@yahoo.com>
2020-09-28 13:57:41 -0700

    comment orderchecker and palindrome perm

--- codeProblems/OrderChecker.java
+++ codeProblems/OrderChecker.java
@@ -3,6 +3,21 @@ import java.util.ArrayList;
 
 
 
+// My cake shop is so popular, I'm adding some tables and hiring wait staff so folks can have a cute sit-down cake-eating experience.
+
+// I have two registers: one for take-out orders, and the other for the other folks eating inside the cafe. All the customer orders get 
+// combined into one list for the kitchen, where they should be handled first-come, first-served.
+
+// Recently, some customers have been complaining that people who placed orders after them are getting their food first. Yikes—that's not good for 
+// business!
+
+// To investigate their claims, one afternoon I sat behind the registers with my laptop and recorded:
+
+// The take-out orders as they were entered into the system and given to the kitchen. (takeOutOrders)
+// The dine-in orders as they were entered into the system and given to the kitchen. (dineInOrders)
+// Each customer order (from either register) as it was finished by the kitchen. (servedOrders)
+
+
 public class OrderChecker
 {
 
@@ -12,27 +27,59 @@ public class OrderChecker
     {
        
 
+        //if the takeout and dinein orders are not the same size as 
+        //the served then we return false because their is an extra order 
+        //or there is a missing order
+        if( takeOut.length + dineIn.length != served.length)
+        {
+            return false;
+        }
+
+        //we set three variable pointers to move along each of our arrays
         int takeOutIndex=0;
         int dineInIndex=0;
         int servedIndex=0;
+
+        // a variable for the size of the served array 
         int servedSize = served.length - 1;
 
+        //we move along until we reach the each of our served array
         while(servedIndex < servedSize)
         {
 
-            boolean takeoutisnotexahsted = takeOutIndex <= takeOut.length-1;
-            boolean dineinisnotexahsted = dineInIndex <= dineIn.length-1;
 
-            if( dineinisnotexahsted == true && served[servedIndex] == dineIn[dineInIndex])
+            //another way to check to see if there are still elements in our array 
+            //but we are not using these
+            // boolean takeoutisnotexahsted = takeOutIndex <= takeOut.length-1;
+            // boolean dineinisnotexahsted = dineInIndex <= dineIn.length-1;
+
+
+            //in this block we are dealing with the dineIn array and seeing if the current index we are at for the
+            //served and the dine in coincide 
+            //in order for this block to run we need to have to make sure that there is still elements in 
+            //the array and that the current index in the served coincides with the current index in the dine in array
+            if( dineInIndex <= dineIn.length-1 && served[servedIndex] == dineIn[dineInIndex])
             {
+
+                //we go ahead and move forward with the served and the dine in array 
                 dineInIndex += 1;
                 servedIndex += 1;
             }
-            else if( takeoutisnotexahsted == true && served[servedIndex] == takeOut[takeOutIndex] )
+
+
+            //in this block we are dealing with the takeout array and seeing if the current index we are at for the
+            //served and the takeout coincide 
+            //in order for this block to run we need to have to make sure that there is still elements in 
+            //the array and that the current index in the served coincides with the current index in the takeout array
+            else if( takeOutIndex <= takeOut.length-1 && served[servedIndex] == takeOut[takeOutIndex] )
             {
+
+                //we go ahead and move forward with the served and the take our array 
                 takeOutIndex += 1;
                 servedIndex += 1;
             }
+
+            //if neither of the conditions are met above 
             else
             {
                 return false;
@@ -56,9 +103,9 @@ public class OrderChecker
 
         OrderChecker program = new OrderChecker();
     
-        int takeOutOrders[]= {111, 13, 5615};
-        int dineInOrders[]= {234, 14, 16};
-        int servedOrders[]= {234,111, 5615,13, 14,16};
+        int takeOutOrders[]= {1, 3, 5};
+        int dineInOrders[]= {2, 4, 6};
+        int servedOrders[]= {1,2, 4,6, 5,3};
 
         System.out.println(program.in_order_order_checker(takeOutOrders,dineInOrders,servedOrders));
 
--- codeProblems/PalindromePermutation.java
+++ codeProblems/PalindromePermutation.java
@@ -1,6 +1,11 @@
 import java.util.*; 
 
 
+
+// Write an efficient method that checks whether any permutation  of an input string is a palindrome. 
+
+// You can assume the input string only contains lowercase letters.
+
 public class PalindromePermutation
 {
 
@@ -9,17 +14,24 @@ public class PalindromePermutation
 	{
 
 
-       
+       //use a set to check if there is only one letter left when the string 
+       //finishes looping that way we know that there is in fact at least one permutation of 
+       //a palindrome
        Set<Character> unpaired_character = new HashSet<>();
 
 
+       //loop throught the string
        for( int i = 0 ; i< the_string.length();i++)
-       {
+       {      
 
+                     //if the character is in the set then we remove it
        		if( unpaired_character.contains(the_string.charAt(i)))
        		{
        			unpaired_character.remove(the_string.charAt(i));
        		}
+                     //or we add it if its not
+                     //this also means that any odd number of characters will
+                     //leave one chracter in the set
        		else
        		{
        			unpaired_character.add(the_string.charAt(i));
@@ -27,6 +39,9 @@ public class PalindromePermutation
 
        }
 
+       //if there is only one chracter left in the set that means 
+       //that we have the abolity to make a permutation of a palindrome 
+       //so we return false or true here  
        return unpaired_character.size() == 1;
 
 
