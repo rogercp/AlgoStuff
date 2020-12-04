4a0de6efc93c22f88647cbf6a597e18b500baaf7
rogercp <rogercp1234@yahoo.com>
2020-12-03 23:30:10 -0800

    rob no rob comm

--- codeProblems/HouseRobber.java
+++ codeProblems/HouseRobber.java
@@ -22,21 +22,27 @@ class HouseRobber {
     public int rob(int[] nums) 
     {
 
+        //set variable for total amount of money you can make for one iteration of robbing
          int rob = 0;
          
+         //set variable for total amount of money you can make for one iteration of not robbing
          int noRob = 0;
         
+        //iterate through the array of houses with the amounts
         for (int n: nums) 
-            
         {
+            //hold current amount in t
             int t = rob;
             
+            //set rob to the new value 
             rob = noRob + n;
             
+            //greedily check to see if the current iteration value is greater than what is already in norob
             noRob = Math.max(noRob, t);
             
         }
         
+        //check to see which value is greater and return it
         return Math.max(rob, noRob);
         
     }
