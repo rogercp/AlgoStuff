099a3f84194062a6508045a70b803adb5a7fa33f
rogercp <rogercp1234@yahoo.com>
2021-01-18 23:33:57 -0800

    started Richest Customer set up problem

---
+++ codeProblems/RichestCustomer.java
@@ -0,0 +1,44 @@
+
+
+import java.util.*;
+
+
+
+public class RichestCustomer { 
+  
+    static int findRichestCustomer(int[][] vals) 
+    {
+
+
+    	int theOne = 1;
+
+
+
+
+
+
+
+
+    	return theOne; 
+    }
+   
+
+    public static void main(String args[]) 
+    { 
+        RichestCustomer program = new RichestCustomer(); 
+
+		int[][] twoDArr={{1,5},{7,3},{3,5}};
+
+        int returnval = program.findRichestCustomertwoDArr();
+
+        System.out.println(returnval);
+
+
+        // for(int i= 0;i<twoDArr.length;i++ )
+        // {
+        // 	 System.out.println(twoDArr[i][0]);
+        // }
+
+       
+    } 
+} 
