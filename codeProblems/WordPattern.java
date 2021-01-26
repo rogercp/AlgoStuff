3123954c070d6afaf697308cf163b0f4420ce60f
rogercp <rogercp1234@yahoo.com>
2021-01-26 00:43:33 -0800

    Word Pattern

---
+++ codeProblems/WordPattern.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+
+
+
+public class WordPattern{ 
+  
+    static boolean wordPatternTrueOrFalse(String pattern, String s ) 
+    { 
+
+
+      
+
+    }
+   
+
+    public static void main(String args[]) 
+    { 
+        WordPattern program = new WordPattern(); 
+
+        boolean returnval = program.wordPatternTrueOrFalse("abba","dog cat cat dog");
+
+        System.out.println(returnval);
+
+
+       
+    } 
+} 
