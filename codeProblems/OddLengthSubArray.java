f69741507ce87d899b5a62013df6d64ee2824537
rogercp <rogercp1234@yahoo.com>
2021-02-08 23:53:49 -0800

    odd length subarray problem

---
+++ codeProblems/OddLengthSubArray.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+
+
+
+public class OddLengthSubArray { 
+  
+    static int sumOddLengthSubarrays(int[] vals) 
+    {
+
+
+    }
+   
+
+    public static void main(String args[]) 
+    { 
+        OddLengthSubArray program = new OddLengthSubArray(); 
+
+		int[][] twoDArr={{2,8,7},{7,1,3},{1,9,5}};
+
+        int returnval = program.findRichestCustomer(twoDArr);
+
+        System.out.println(returnval);
+
+
+       
+    } 
+} 
