9909bd66f66b033d3d522eab1ee6f48434cf783a
rogercp <rogercp1234@yahoo.com>
2021-02-10 00:01:02 -0800

    using two while loops to find odd sub array accumulation

--- codeProblems/OddLengthSubArray.java
+++ codeProblems/OddLengthSubArray.java
@@ -2,13 +2,44 @@
 
 import java.util.*;
 
+// Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
 
+// A subarray is a contiguous subsequence of the array.
+
+// Return the sum of all odd-length subarrays of arr.
 
 public class OddLengthSubArray { 
   
     static int sumOddLengthSubarrays(int[] vals) 
     {
 
+        int accumulator = 0;
+        int oddPointer = 1;
+
+
+        while (oddPointer <= vals.length)
+        {
+
+        int innerPointer = 0;
+        innerAccumulator = 0;
+
+          while(innerPointer <=oddPointer)
+          {
+
+            innerAccumulator += vals[innerPointer];
+
+          }
+
+
+          oddPointer = oddPointer + 2;
+          accumulator += innerAccumulator;
+
+
+
+        }
+
+
+        return accumulator;
 
     }
    
