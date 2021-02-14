c826afaad884735ac24980dabb6911e69aa2efcf
rogercp <rogercp1234@yahoo.com>
2021-02-13 16:10:14 -0800

    word cloud problem

---
+++ codeProblems/WordCloud.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+
+// You want to build a word cloud, an infographic where the size of a word corresponds to how 
+// often it appears in the body of text.
+
+// To do this, you'll need data. Write code that takes a long string and builds its word cloud 
+// data in a hash map ↴ , where the keys are words and the values are the number of times the words occurred.
+
+public class WordCloud{ 
+  
+    static boolean wordCloudData(String pattern, String s ) 
+    { 
+
+
+    }
+   
+
+    public static void main(String args[]) 
+    { 
+        WordCloud program = new WordCloud(); 
+
+        boolean returnval = program.wordCloudData("abba","dog cat cat dog");
+
+        System.out.println(returnval);
+
+
+    } 
+} 
