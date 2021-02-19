ec956eeb8dcdfd455bc5ff63b532113ff58586bc
rogercp <rogercp1234@yahoo.com>
2021-02-18 23:00:55 -0800

    caketheif problem

---
+++ codeProblems/CakeTheif.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+
+
+public class CakeTheif
+{
+
+   final int weight;
+  final int value;
+
+    public CakeType(int weight, int value) 
+    {
+        this.weight = weight;
+        this.value  = value;
+    }
+
+
+    public boolean maxDuffelBagValue(int cakeTypes[], int capacity)
+    {
+     
+
+    }
+
+
+
+
+
+    public static void main(String[] args)
+    {
+
+        CakeType[] cakeTypes = new CakeType[]
+         {
+          new CakeType(7, 160),
+          new CakeType(3, 90),
+          new CakeType(2, 15),
+          };
+
+        CakeTheif program = new CakeTheif();
+    
+        int movietimes[]= {23,456,100,23423,130,34,465};
+
+        System.out.println(program.can_two_movies_fill_flight(movietimes,230));
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
