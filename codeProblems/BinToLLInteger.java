d11aedd0ed7302cd0fe382b7d2b702ebb5c1ea50
rogercp <rogercp1234@yahoo.com>
2021-03-09 10:57:35 -0800

    problem converting binary number to linked list integer

---
+++ codeProblems/BinToLLInteger.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+
+public class BinToLLInteger
+{
+
+     public int getDecimalValue(ListNode head) 
+    {
+       
+
+
+    }
+
+
+
+    public static void main(String[] args)
+    {
+
+
+        BinToLLInteger program = new BinToLLInteger();
+    
+
+        System.out.println();
+
+
+
+
+    }
+
+
+
+}
+
+
+ 
+
+
+
+
+
+
+
+
